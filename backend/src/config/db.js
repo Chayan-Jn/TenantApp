0c53485b8ecf74f08531a78848e962ad546ade98
chayan <chayanj39@gmail.com>
2026-04-17 23:28:18 +0530

    Fixed db giving ca certificate problem

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,18 @@
 import pg from 'pg';
+import path from 'path';
+import fs from 'fs'
 const { Pool } = pg;
 import {env} from './env.js'
 
+const certPath = path.resolve(process.cwd(), 'certs/ca-certificate.crt');
+
 const pool = new Pool({
   connectionString: env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true, 
+    ca: fs.readFileSync(certPath).toString() // Reading the digital ID badge
+  }
 });
-
 export const connectToDB = async () => {
   try {
     await pool.query('SELECT 1');
