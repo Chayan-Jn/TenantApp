addecd1b3ee1a2ac84925d62b5d4017a1477a8ed
chayan <chayanj39@gmail.com>
2026-04-18 00:05:57 +0530

    cert issue fix

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,26 +1,32 @@
 import pg from 'pg';
 import path from 'path';
-import fs from 'fs'
+import fs from 'fs';
 const { Pool } = pg;
-import {env} from './env.js'
+import { env } from './env.js';
 
 const certPath = path.resolve(process.cwd(), 'certs/ca-certificate.crt');
-const caCert = process.env.CA_CERT 
-  ? env.CA_CERT 
-  : fs.readFileSync(certPath).toString();
+let caCertString = '';
+if (env.CA_CERT && env.CA_CERT.includes('BEGIN CERTIFICATE')) {
+  caCertString = env.CA_CERT.replace(/\\n/g, '\n');
+} else if (fs.existsSync(certPath)) {
+  caCertString = fs.readFileSync(certPath).toString();
+}
 
 const pool = new Pool({
   connectionString: env.DATABASE_URL,
+  max: 12, 
+  connectionTimeoutMillis: 5000, 
+  
   ssl: {
     rejectUnauthorized: true,
-    ca: caCert
+    ca: caCertString
   }
 });
 
 export const connectToDB = async () => {
   try {
     await pool.query('SELECT 1');
-    console.log('DB connected successfully');
+    console.log('DB connected successfully via Pool');
   } catch (err) {
     console.error('Error connecting to db', err);
     throw err;
